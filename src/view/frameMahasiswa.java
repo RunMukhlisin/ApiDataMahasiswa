f62e643aba7163886e43722b87ed3d35e6192ea3
ASUS <ASUS@SayMukhlisin>
2024-11-19 18:36:55 +0700

    bismillah first and last

--- src/view/frameMahasiswa.java
+++ src/view/frameMahasiswa.java
@@ -69,8 +69,6 @@ public class frameMahasiswa extends javax.swing.JFrame {
         tbMahasiswa = new javax.swing.JTable();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        bPrev = new javax.swing.JButton();
-        bNext = new javax.swing.JButton();
         bClose = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -111,20 +109,6 @@ public class frameMahasiswa extends javax.swing.JFrame {
             .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
         );
 
-        bPrev.setText("Prev");
-        bPrev.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                bPrevActionPerformed(evt);
-            }
-        });
-
-        bNext.setText("Next");
-        bNext.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                bNextActionPerformed(evt);
-            }
-        });
-
         bClose.setText("Close");
         bClose.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -138,15 +122,11 @@ public class frameMahasiswa extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 1316, Short.MAX_VALUE)
                 .addContainerGap())
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(808, Short.MAX_VALUE)
-                .addComponent(bPrev)
-                .addGap(53, 53, 53)
-                .addComponent(bNext)
-                .addGap(52, 52, 52)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(bClose)
                 .addGap(199, 199, 199))
         );
@@ -157,10 +137,7 @@ public class frameMahasiswa extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(bPrev)
-                    .addComponent(bNext)
-                    .addComponent(bClose))
+                .addComponent(bClose)
                 .addContainerGap(46, Short.MAX_VALUE))
         );
 
@@ -180,25 +157,6 @@ public class frameMahasiswa extends javax.swing.JFrame {
         
     }//GEN-LAST:event_tbMahasiswaMouseClicked
 
-    private void bNextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bNextActionPerformed
-        // TODO add your handling code here:
-        if (mahasiswa.getBaris()>0 && mahasiswa.getBaris()<=mahasiswa.getDataMhs().length()) {
-            mahasiswa.setBaris(mahasiswa.getBaris()+1);
-            loadTable();
-            
-        }
-    }//GEN-LAST:event_bNextActionPerformed
-
-    private void bPrevActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bPrevActionPerformed
-        // TODO add your handling code here:
-        if (mahasiswa.getBaris()>0 && mahasiswa.getBaris()<=mahasiswa.getDataMhs().length()) {
-            mahasiswa.setBaris(mahasiswa.getBaris()+1);
-            loadTable();
-            
-        }
-
-    }//GEN-LAST:event_bPrevActionPerformed
-
     /**
      * @param args the command line arguments
      */
@@ -213,8 +171,6 @@ public class frameMahasiswa extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton bClose;
-    private javax.swing.JButton bNext;
-    private javax.swing.JButton bPrev;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
